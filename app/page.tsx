0e92c7b6675ac22bea1f153c5e30cefd4ea87d9d
Armaan Seydi <armaanseydi@gmail.com>
2024-04-25 17:02:18 +0330

    AboutSection done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "../components/HeroSection";
 import TechStackSection from "@/components/TechStackSection";
+import AboutSection from "@/components/AboutSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <TechStackSection />
+        <AboutSection />
       </div>
     </main>
   );
---
+++ components/AboutSection.tsx
@@ -0,0 +1,97 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>JavaScript</li>
+        <li>HTML/CSS</li>
+        <li>TypeScript</li>
+        <li>React</li>
+        <li>Git</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Front-end Bootcamp of Quera</li>
+        <li>Quera Collage Front-end course</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Task Oriented Bootcamp passed with perfect Score.</li>
+        <li>Internship at Quera</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id: string) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image
+          src="/images/about-image.jpg"
+          width={500}
+          height={500}
+          alt="About me"
+          className="grayscale brightness-100 contrast-100 hue-rotate-0 saturate-100 sepia-0 filter"
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a Front-edn web developer with a passion for creating interactive and responsive
+            web applications. I have experience working with JavaScript, Typescript, React, Redux,
+            Tailwind, Material-UI, Sass, HTML, CSS, and Git. I am a quick learner and I am always
+            looking to expand my knowledge and skill set. I am a team player and I am excited to
+            work with others to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab)?.content ?? <div>No content available</div>}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
