49b233330642cddd6197c7995966333559932c6d
Armaan Seydi <armaanseydi@gmail.com>
2024-04-25 17:06:40 +0330

    Fix: typo in AboutSection

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -32,7 +32,7 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>Task Oriented Bootcamp passed with perfect Score.</li>
+        <li>Task Oriented Bootcamp passed with perfect Score</li>
         <li>Internship at Quera</li>
       </ul>
     ),
