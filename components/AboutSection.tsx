8a3f9116f356bc622f6c444e2484a4ed10c8728a
Armaan Seydi <armaanseydi@gmail.com>
2024-04-27 15:43:53 +0330

    chores

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -62,7 +62,7 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a Front-edn web developer with a passion for creating interactive and responsive
+            I am a Front-end web developer with a passion for creating interactive and responsive
             web applications. I have experience working with JavaScript, Typescript, React, Redux,
             Tailwind, Material-UI, Sass, HTML, CSS, and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and I am excited to
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -37,7 +37,8 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptuous.
+            a Iranian Game & Anime lover Who enjoys making stuff and breaking down the stuff that
+            others made.
           </p>
           <div>
             <Link
@@ -47,11 +48,11 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="/#projects"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                Projects
               </span>
             </Link>
           </div>
